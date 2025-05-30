fe586f977c5ff3570d44db11ac67907a48045c31
lgxi24 <lambert.lgx@gmail.com>
2025-05-30 14:39:54 +0100

    modify answer type

--- src/chatbot.py
+++ src/chatbot.py
@@ -4,7 +4,6 @@ from ibm_watsonx_ai.foundation_models import ModelInference
 import os
 from dotenv import load_dotenv
 
-# Load environment variables from .env file
 load_dotenv()
 
 watsonx_ai_url = os.getenv("WATSONX_AI_URL")
@@ -12,12 +11,8 @@ apikey = os.getenv("WATSONX_API_KEY")
 project_id = os.getenv("WATSONX_PROJECT_ID")
 model_id = os.getenv("WATSONX_MODEL_ID")
 
-params = {
-  "decoding_method": "greedy",
-  "temperature": 0.8,
-  "min_new_tokens": 10,
-  "max_new_tokens": 100
-}
+if not all([watsonx_ai_url, apikey, project_id, model_id]):
+    raise ValueError("Missing required environment variables. Please check your .env file.")
 
 credentials = Credentials(
     url = watsonx_ai_url,
@@ -25,18 +20,43 @@ credentials = Credentials(
 )
 
 client = APIClient(credentials)
+    
+params = {
+    "decoding_method": "greedy",
+    "temperature": 0.7,
+    "max_new_tokens": 300
+}
 
 model = ModelInference(
-  model_id=model_id,
-  api_client=client,
-  params=params,
-  project_id=project_id,
-  space_id=None,
-  verify=False,
+    model_id=model_id,
+    api_client=client,
+    project_id=project_id,
+    space_id=None,
+    verify=False,
+    params=params
 )
 
-prompt = "How far is Paris from Bangalore?"
-stream_response = model.generate_text_stream(prompt, params)
 
-for chunk in stream_response:
-  print(chunk, end='')
+
+def get_response(prompt: str) -> str:
+    try:
+        response = model.generate_text(
+            prompt=prompt
+        )
+        response = response.split("\n")    
+        # if the model only helps complete the prompt (not start with capital letter), 
+        # then we need to include the next line
+        if not response[0][0].isupper():
+            return response[1]
+        else:
+            return response[0]
+    
+    except Exception as e:
+        print(f"Error: {str(e)}")
+        return "An error occurred while generating the response."
+
+if __name__ == "__main__":
+    prompt = input("Enter your prompt: ")
+    templated_prompt = f"Give your answer in 5 sentences. {prompt}"
+    response = get_response(templated_prompt)
+    print(f"\n\nResponse: \n{response}")
